0fd94644c13f6452871310be68f99f7df9c94062
Mansi6396 <mansitrivedi315@gmail.com>
2022-12-22 13:24:28 +0530

    task 1 done

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './Components/Page1/Page1';
+import Page2 from './Components/Page2/Page2';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Page1/>}/>
+        <Route path='/details/:id' element={<Page2/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ demo/src/Components/Page1/Page1.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+ import "./Page1.css"
+
+import { useNavigate } from "react-router-dom";
+import Grid from '@mui/material/Grid';
+
+
+export default function Page1() {
+  const [subData, setSubData] = useState([]);
+  const navigate = useNavigate();
+  const getData = async() => {
+   await axios
+      .get(`https://swapi.dev/api/films`)
+
+      .then((res) => {
+        console.log("REeeeeeS::", res?.data?.results);
+        setSubData(res?.data?.results);
+       
+        // setSubData(res?.data)
+        // console.log('subdata',res?.data);
+      })
+      .catch((err) => {
+        return err;
+      });
+  };
+  // const EffectData = useRef(true)
+
+  useEffect(() => {
+    // if (EffectData.current) {
+        // EffectData.current = false;
+        getData();
+    // }
+    
+  },[]);
+
+ 
+
+  const MoreDetails = ()=>{
+    navigate('/details')
+  }
+
+  return (
+    <>
+    <div className="bgcolor" style={{ backgroundColor:"#f36576",marginTop:"-1.5%"}}>
+     <h1>Star War Movies</h1>
+     <div style={{backgroundColor:" #e0efff",fontSize:"1.5rem",marginBottom:"2%"}}>
+     <marquee behavior="scroll" direction="left" Scrollamount="10">In a dark place we find ourselves and a little more knowledge lights our way.!</marquee>
+     </div>
+     <div className="main" >
+     
+     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
+        {subData.map((val,index) => {
+          console.log("value", val);
+          console.log("index", index);
+          return (
+            <>
+            
+            <Grid item xs={2} sm={4} md={4} >
+        <div className="card" >
+        <div>Name:{val.title}</div>
+              <div>Episode Number:{val.episode_id}</div>
+              <div>Director:{val.director}</div>
+              <div>Relesed-date:{val.release_date}</div>
+              <button className="button" onClick={()=>navigate(`/details/${index + 1}`)}>See More Details</button>
+              {/* <Button color="primary" variant="contained"  >See More Details</Button> */}
+        </div>
+        </Grid>
+            </>
+          );
+        })}
+        </Grid>
+      </div>
+      </div>
+    </>
+  );
+}
